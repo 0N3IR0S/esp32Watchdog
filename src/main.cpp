40f69bc30b5c563c74cc0445fe4e4c5506b2fc8e
Benjamin Buerge <benjamin.buerge@buhlergroup.com>
2020-10-12 08:46:35 +0200

    simple test program

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println(F("Serial Monitor started"));
   // put your setup code here, to run once:
 }
 
 void loop() {
+  Serial.println("Hello");
+  delay(1000);
   // put your main code here, to run repeatedly:
 }
